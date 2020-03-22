02599e9d32daf5a995b2df35293c77207173f7c2
alfredoav2 <alfredo.almazan@saei.mx>
2020-03-21 18:15:09 -0600

    2

--- pruebas/pruebas/pruebas.cpp
+++ pruebas/pruebas/pruebas.cpp
@@ -325,8 +325,38 @@ int hola(std::vector<Vector2*>& ver) {
 		}
 	}
 }
+
 int main(int argc, char* args[])
 {
+	
+	/*# dibujo
+	hay 6 cuadros en la izquierda de la pantalla
+
+	caja 1 es el boton para dibujar un solo punto
+	-para dibujar un solo punto primero aga clic en la primera caja (caja  numero 1 contando de arriba hacia abajo)
+	-despues aga clic en cualquier parte de la pantalla que no sea el boton para dibujar un punto
+
+	caja 2 es el boton para dibujar una linea
+	-para dibujar una linea primero aga clic en la segunda caja (caja numero 2 contando de arriba hacia abajo)
+	-despues haga clic en dos lugares diferentes de la pantalla (la linea se dibujara despues del segundo clic)
+
+	caja 3 es el boton para dibujar un cuadrado
+	-para dibujar un cuadrado primero aga clic en la tercera caja (caja numero 3 contando de arriba hacia abajo)
+	-despues haga clic en 4 partes diferentes de la pantalla (el cuadrado se dibujara despues del cuarto clic)
+
+	caja 4 es el boton para dibujar un circulo
+	-para dibujar un circulo primero aga clic en la cuarta caja (caja numero 4 contando de arriba hacia abajo)
+	-despues aga clic en dos puntos diferentes de la pantalla, el segundo clic es el centro del sirculo y
+	y tambien sirve para calcular la distancia entre el primer clic y el segundo clic esto nos da el radio,
+	(despues del segundo clic se dibujara el circulo)
+
+	caja 5 es el boton para dibujar una linea de Bezier
+	-para dibujar una linea de Bezier primero aga clic en la quinta caja (caja numero 5 contando de arriba hacia abajo)
+	-despues haga cuantos clics quiera en la pantalla (preferiblemente simetricos) cuando quiera que se dibuje la linea
+	oprima cualquier tecla despues de hacer los clics que quiera.
+
+	 caja 6 es el boton para borrar todo
+	-si oprime el boton borrara todos los dibujos*/
 	SetScreen();
 	std::vector<Vector2*>ver;
 	bool linea = false;
